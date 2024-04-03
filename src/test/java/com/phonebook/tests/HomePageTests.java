f8893b8232da0a2b775757c97c37181fce202579
mmaksimuss <mmaksimus1977@gmail.com>
2024-04-03 20:10:47 +0200

    update

---
+++ src/test/java/com/phonebook/tests/HomePageTests.java
@@ -0,0 +1,35 @@
+package com.phonebook.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class HomePageTests {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://telranedu.web.app");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterMethod(enabled = false)
+    public void tearDown() {
+        driver.quit();
+    }
+    @Test
+    public void isHomeComponenetPresent(){
+        driver.findElement(By.cssSelector("div:nth-child(2) div h1"));
+    }
+
+
+
+
+}
